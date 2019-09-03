531b0b9ed0c2973b7c17d4e2036f75209865137f
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2019-09-03 20:09:22 +0530

    Create linear_search.c

---
+++ data_structure_lab/linear_search.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int arr[20],pos,key,num,i=0,j,flag=0;
+	clrscr();
+	printf("Enter the no of elements:");
+	scanf("%d",&num);
+
+	while(i<num){
+		printf("Enter:");
+		scanf("%d",&arr[i]);
+		i++;
+	}
+
+	printf("Enter the value to be searched:");
+	scanf("%d",&key);
+
+	for(j=0;j<num;j++){
+
+		if(arr[j] == key)
+		{
+			flag=1;
+			pos = j+1;
+			break;
+		}
+
+	}
+	if(flag == 1)
+		printf("Element found in the %dth position",pos);
+	else
+		printf("Element not found");
+	getch();
+}
