2bdad9407d018675d68897e6d9ffe42f5bfe2a3b
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2019-09-11 21:58:38 +0530

    Create substring_identification.c

---
+++ data_structure_lab/substring_identification.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char str[20],sub[20];
+    int count1=0,count2=0,i,j,flag;
+
+    clrscr();
+
+    printf("enter the main string:\n");
+    gets(str);
+
+    printf("enter substring:\n");
+    gets(sub);
+
+    count1 = strlen(str);
+    count2 = strlen(sub);
+
+    for(i=0;i<=count1-count2;i++)
+     {       flag=1;
+
+	for(j=i;j<i+count2;j++)
+	 {
+	     if(str[j]!=sub[j-i])
+	     {
+		 flag=0;
+		 break;
+	     }
+	 }
+
+	  if(flag==1)
+	     break;
+    }
+       if(flag==1)
+
+	    printf("substring present");
+       else
+	    printf("substring not present");
+   getch();
+   }
