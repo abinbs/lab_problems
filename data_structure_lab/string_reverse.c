c6edc85459871bb6648d2d94aefa25e331e329c5
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2019-09-13 17:20:13 +0530

    Create string_reverse.c

---
+++ data_structure_lab/string_reverse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main(){
+	char str[20],nstring[20];
+	int len,i,j=0;;
+	clrscr();
+	printf("Enter a string:");
+	gets(str);
+	len = strlen(str);
+	for(i = len-1 ;i>=0;i--){
+
+		nstring[j] = str[i];
+		j++;
+	}
+        nstring[len] = '\0';
+	printf("String after reversal::: ");
+	puts(nstring);
+	getch();
+
+
+}
