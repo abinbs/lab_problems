f5e50f4a380a907a2833d6f4e8718b2e4137d2a3
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2019-09-26 20:42:13 +0530

    Update add_beg_linked_list.c

--- data_structure_lab/add_beg_linked_list.c
+++ data_structure_lab/add_beg_linked_list.c
@@ -1,75 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+//#include<conio.h>   // not needed while using an online compiler
+#include<stdlib.h>
+//#include<process.h>
 
+struct node{
+	int data;
+	struct node* link;
+}*header,*ptr,*temp,*ptr1;
 
-struct Node {
-    int data;
-    struct Node *link;
-}*header;
-void createList(int num);
+void add_beg();
 void Print();
-void add_beg(int n);
+void initialise(){
+	header=(struct node*)malloc(sizeof(struct node));
+	header->link=NULL;
+}
+void main(){
 
-void initialize(){
-    header=(struct Node*)malloc(sizeof(struct Node));
-    header->link=NULL;
+
+	int num,i,n,ch,key;
+	//clrscr();
+	initialise();
+
+	 while(1){
+	 printf("Enter the choices \n 1.add beg\n2.Exit\n");
+	 scanf("%d",&ch);
+
+	 switch(ch){
+		case 1:
+		add_beg(n);
+
+		break;
+		
+		case 2: exit(1);
+		default: printf("Wrong Choice");
+	 }
+
+       }
+
+	 //getch();
 }
 
-
-int main()
-{
-    
-    
-    initialize();
-    int num=0,i,val=0,n;
-    printf("Enter the number of items");
-    scanf("%d",&val);
-    printf("Enter the numbers");
-    for(i=0;i<val;i++){
-        scanf("%d",&num);
-        createList(num);
-    }
-    Print();
-    printf("Enter the number");
-    scanf("%d",&n);
-    add_beg(n);
-    Print();
-
-}
-void createList(int num){
-
-        struct Node *q,*temp;
-        q=header;
-        while(q->link!=NULL){
-            q=q->link;
-        }
-        temp=(struct Node*)malloc(sizeof(struct Node));
-        temp->data=num;
-        temp->link=NULL;
-        q->link=temp;
-    
-}
 void Print(){
-    struct Node *ptr;
-    ptr=header->link;
-    printf("List is :");
-    while(ptr!= NULL){
-        printf("  %d",ptr->data);
-        ptr=ptr->link;
-    }
-    printf("\n");
+	ptr=header->link;
+	printf("List is: ");
+	while(ptr!=NULL){
+		printf(" %d",ptr->data);
+		ptr=ptr->link;
+
+	}
 }
-void add_beg(int n){
-    struct Node *temp,*ptr;
-    printf("%d",n);
-    ptr=header;
-    temp=(struct Node*)malloc(sizeof(struct Node));
-    if(temp==NULL){
-        printf("Memory Underflow");
-        exit(0);
-    }
-    temp->data=n;
-    temp->link=header->link;
-    header->link=temp;
-    
+void add_beg(){
+	char ch,c;
+	int n;
+	ptr=header;
+	ch='y';
+	while(ch=='y'){
+	printf("Enter the value to be added\n");
+	scanf("%d",&n);
+	temp=(struct node*)malloc(sizeof(struct node));
+	if(temp==NULL){
+		printf("Memory insufficient");
+		exit(0);
+		}
+	temp->data=n;
+	temp->link=header->link;
+	header->link=temp;
+	Print();
+	fflush(stdin);
+    while((c = getchar()) != '\n' && c != EOF);
+	printf("do you want to continue (y/n)");
+	scanf("%c",&c);
+	ch=c;
+	}
 }
